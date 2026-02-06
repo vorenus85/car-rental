20f69b043fda742d2e5db37c6683f531bc78b35b
Janos Perge <janos.perge@gmail.com>
2026-02-06 18:46:30 +0100

    add admin layout

---
+++ resources/js/layouts/AppSideBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="layout-sidebar shadow">
+        <ul class="sidebar-menu">
+            <template v-for="menu in menus" :key="menu.title">
+                <SidebarMenuitem
+                    :icon="menu.icon"
+                    :route-name="menu.routeName"
+                    :title="menu.title"
+                />
+            </template>
+        </ul>
+    </div>
+</template>
+<script setup>
+import SidebarMenuitem from '@/components/SidebarMenuitem.vue'
+
+const menus = [
+    {
+        icon: 'home',
+        routeName: 'dashboard',
+        title: 'Dashboard',
+    },
+    {
+        icon: 'bookmark',
+        routeName: 'bookings',
+        title: 'Bookings',
+    },
+    {
+        icon: 'car',
+        routeName: 'units',
+        title: 'Units',
+    },
+    {
+        icon: 'calendar',
+        routeName: 'calendar',
+        title: 'Calendar',
+    },
+    {
+        icon: 'users',
+        routeName: 'clients',
+        title: 'Clients',
+    },
+    {
+        icon: 'cog',
+        routeName: 'settings',
+        title: 'Settings',
+    },
+    {
+        icon: 'sign-out',
+        routeName: 'logout',
+        title: 'Logout',
+    },
+]
+</script>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,13 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomePage from '../Pages/HomePage.vue'
-import AboutPage from '../Pages/AboutPage.vue'
+import DashboardPage from '@/pages/DashboardPage.vue'
+import BookingsPage from '@/pages/BookingsPage.vue'
+import UnitsPage from '@/pages/UnitsPage.vue'
+import CalendarPage from '@/pages/CalendarPage.vue'
+import ClientsPage from '@/pages/ClientsPage.vue'
+import SettingsPage from '@/pages/SettingsPage.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', name: 'home', component: HomePage },
-        { path: '/about', name: 'about', component: AboutPage },
+        { path: '/', name: 'dashboard', component: DashboardPage },
+        { path: '/bookings', name: 'bookings', component: BookingsPage },
+        { path: '/units', name: 'units', component: UnitsPage },
+        { path: '/calendar', name: 'calendar', component: CalendarPage },
+        { path: '/clients', name: 'clients', component: ClientsPage },
+        { path: '/settings', name: 'settings', component: SettingsPage },
+        {
+            path: '/logout',
+            name: 'logout',
+            meta: { requiresAuth: true },
+        },
     ],
 })
 
