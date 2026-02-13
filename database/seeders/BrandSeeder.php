eb6c8979e7773a9f417f1c582edd868d90b0510b
Janos Perge <janos.perge@gmail.com>
2026-02-13 15:30:57 +0100

    fix brand seeder

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -22,7 +22,7 @@ class BrandSeeder extends Seeder
         foreach ($data as $item) {
             Brand::create([
                 'name' => $item['name'],
-                'icon' => $item['icon'],
+                'logo' => $item['logo'],
             ]);
         }
 
