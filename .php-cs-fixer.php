7f2bf0a1594339795001ef6424f8f9a25404c100
Janos Perge <janos.perge@gmail.com>
2026-04-14 10:59:17 +0200

    fix php formatter

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -7,7 +7,16 @@ $finder = PhpCsFixer\Finder::create()
 return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR12' => true,
+
         'array_syntax' => ['syntax' => 'short'],
         'binary_operator_spaces' => ['default' => 'single_space'],
+
+        'ordered_imports' => true,
+        'no_unused_imports' => true,
+
+        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+
+        // optional, but recommended
+        // 'declare_strict_types' => true,
     ])
     ->setFinder($finder);
