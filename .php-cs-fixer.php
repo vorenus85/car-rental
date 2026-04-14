180c447eb3e6fff7e97bf465ea548bb1b058a098
Janos Perge <janos.perge@gmail.com>
2026-04-14 11:09:08 +0200

    exclude 'bootstrap/cache' from formatter

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,22 +1,22 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude(['vendor', 'node_modules', 'public']);
+  ->in(__DIR__)
+  ->exclude(['vendor', 'node_modules', 'public', 'bootstrap/cache',]);
 
 return (new PhpCsFixer\Config())
-    ->setRules([
-        '@PSR12' => true,
+  ->setRules([
+    '@PSR12' => true,
 
-        'array_syntax' => ['syntax' => 'short'],
-        'binary_operator_spaces' => ['default' => 'single_space'],
+    'array_syntax' => ['syntax' => 'short'],
+    'binary_operator_spaces' => ['default' => 'single_space'],
 
-        'ordered_imports' => true,
-        'no_unused_imports' => true,
+    'ordered_imports' => true,
+    'no_unused_imports' => true,
 
-        'trailing_comma_in_multiline' => ['elements' => ['arrays']],
+    'trailing_comma_in_multiline' => ['elements' => ['arrays']],
 
-        // optional, but recommended
-        // 'declare_strict_types' => true,
-    ])
-    ->setFinder($finder);
+    // optional, but recommended
+    // 'declare_strict_types' => true,
+  ])
+  ->setFinder($finder);
