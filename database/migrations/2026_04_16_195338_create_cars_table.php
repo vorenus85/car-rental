8155956ea3594d6fd276c21bad1140433e0ba838
Janos Perge <janos.perge@gmail.com>
2026-04-17 08:16:55 +0200

    refactor car entity seeding

---
+++ database/migrations/2026_04_16_195338_create_cars_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('variant_id')
+                ->constrained('car_variants')
+                ->cascadeOnDelete();
+
+            $table->string('licence_plate')->unique();
+
+            $table->integer('price_per_day');
+            $table->enum('status', ['available', 'unavailable', 'maintenance']);
+
+            $table->integer('production_year');
+            $table->integer('mileage');
+
+            $table->string('image')->nullable();
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
