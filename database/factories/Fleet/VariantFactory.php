e7e061bc106a515e325d190e9cfe2c3349c718e5
Janos Perge <janos.perge@gmail.com>
2026-05-19 15:30:08 +0200

    feat: add VariantFactory for generating Variant model instances

---
+++ database/factories/Fleet/VariantFactory.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Database\Factories\Fleet;
+
+use App\Models\Fleet\CarModel;
+use App\Models\Fleet\Variant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Variant>
+ */
+class VariantFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<Variant>
+     */
+    protected $model = Variant::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(2, true),
+
+            'model_id' => CarModel::factory(),
+
+            'category' => fake()->randomElement([
+                'economy',
+                'compact',
+                'sedan',
+                'suv',
+                'luxury',
+            ]),
+
+            'description' => fake()->sentence(),
+
+            'body_type' => fake()->randomElement([
+                'sedan',
+                'hatchback',
+                'wagon',
+                'suv',
+                'coupe',
+            ]),
+
+            'transmission' => fake()->randomElement([
+                'manual',
+                'automatic',
+            ]),
+
+            'fuel' => fake()->randomElement([
+                'petrol',
+                'diesel',
+                'hybrid',
+                'electric',
+            ]),
+
+            'seats' => fake()->numberBetween(2, 7),
+
+            'doors' => fake()->randomElement([
+                2,
+                3,
+                4,
+                5,
+            ]),
+        ];
+    }
+}
