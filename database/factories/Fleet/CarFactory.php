83663ec830f3a8ec5f3161febda9d070d22a609c
Janos Perge <janos.perge@gmail.com>
2026-05-24 10:22:20 +0200

    refactor: create CarFactory for generating car model instances

---
+++ database/factories/Fleet/CarFactory.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Factories\Fleet;
+
+use App\Models\Fleet\Car;
+use App\Models\Fleet\Variant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Car>
+ */
+class CarFactory extends Factory
+{
+    protected $model = Car::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'variant_id' => Variant::factory(),
+
+            'licence_plate' => strtoupper(fake()->bothify('???-####')),
+
+            'image' => fake()->imageUrl(),
+
+            'price_per_day' => fake()->numberBetween(20, 200),
+
+            'status' => fake()->randomElement([
+                'available',
+                'reserved',
+                'rented',
+                'maintenance',
+                'retired'
+            ]),
+
+            'production_year' => fake()->numberBetween(2015, now()->year),
+
+            'mileage' => fake()->numberBetween(5_000, 250_000),
+
+            'color' => fake()->randomElement([
+                'white',
+                'black',
+                'silver',
+                'gray',
+                'red',
+                'blue',
+                'green',
+                'yellow',
+                'orange',
+                'brown',
+            ]),
+
+            'description' => fake()->optional()->paragraph(),
+        ];
+    }
+}
