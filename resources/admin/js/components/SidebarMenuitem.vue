4907858d9c6c0cd28984ddbc57412e0efa00dab0
Janos Perge <janos.perge@gmail.com>
2026-06-09 13:30:24 +0200

    feat: Enhance admin sidebar toggle functionality and update routing structure

--- resources/admin/js/components/SidebarMenuitem.vue
+++ resources/admin/js/components/SidebarMenuitem.vue
@@ -4,7 +4,7 @@
         {{ title }}</RouterLink
     >
     <template v-else>
-        <div class="sidebar-menuitem-has-child cursor-pointer" @click="toggle">
+        <div class="sidebar-menuitem-has-child cursor-pointer" :class="{ active }" @click="toggle">
             <UiIcon :icon="icon" class="sidebar-menuitem-icon" />
             {{ title }}
             <i class="pi pi-fw pi-angle-down layout-submenu-toggler ml-auto"></i>
@@ -12,16 +12,29 @@
     </template>
 </template>
 <script setup>
+import { ref } from 'vue'
 import UiIcon from '@admin/components/UiIcon.vue'
+
 const emit = defineEmits(['toggle'])
 
 defineProps({
     icon: { type: String, required: true },
     routeName: { type: String, required: false, default: '' },
     title: { type: String, required: true },
+    active: { type: Boolean, default: false },
 })
 
 const toggle = () => {
     emit('toggle')
 }
 </script>
+<style scoped>
+.sidebar-menuitem-has-child .layout-submenu-toggler:before {
+    transition: all 0.2s;
+    display: inline-block;
+}
+
+.sidebar-menuitem-has-child.active .layout-submenu-toggler:before {
+    transform: rotate(180deg);
+}
+</style>
