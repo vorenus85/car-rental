a189617fac9074e8b0575d8e88a2e272d54a9a39
Janos Perge <janos.perge@gmail.com>
2026-06-19 15:32:50 +0200

    feat: reorder car detail route for improved API structure

--- routes/storefront.php
+++ routes/storefront.php
@@ -6,8 +6,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/api/storefront/cars')->group(function () {
     Route::get('/', [CarController::class, 'index']);
-    Route::get('/{car}', [CarController::class, 'show']);
     Route::get('/random-available', [CarController::class, 'randomCars']);
+    Route::get('/{car}', [CarController::class, 'show']);
 });
 
 Route::prefix('/api/storefront/locations')->group(function () {
