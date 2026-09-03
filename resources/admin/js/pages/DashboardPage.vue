684a0502c2a750f36a140e62d5c9ea096b695b74
Janos Perge <janos.perge@gmail.com>
2026-09-03 15:26:07 +0200

    fix: adjust grid layout for dashboard KPIs

--- resources/admin/js/pages/DashboardPage.vue
+++ resources/admin/js/pages/DashboardPage.vue
@@ -1,7 +1,7 @@
 <template>
     <AppLayout>
         <PageTitle title="Dashboard"> </PageTitle>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             <DashboardKpi
                 :value="dashboardKpis?.availableCarsKpi"
                 title="Available Cars"
